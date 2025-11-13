c7afa0c52cfe43990a9fa46ca39a6d4e6dfa46d0
Yuriy Ryabinin <yr.7ce.rn@gmail.com>
2025-11-13 19:45:43 +0300

    fix: check if comment nodes

--- src/rules/empty-line-after-imports.js
+++ src/rules/empty-line-after-imports.js
@@ -7,16 +7,26 @@ module.exports = {
     return {
       ImportDeclaration(node) {
         const nextToken = context.sourceCode.getTokenAfter(node);
+        const comment = context.sourceCode.getCommentsAfter(node)[0];
+        let nextNode;
 
-        if (nextToken) {
-          const isImportNext = nextToken.type === 'Keyword' && nextToken.value === 'import';
+        if (nextToken && !comment) {
+          nextNode = nextToken;
+        } else if (!nextToken && comment) {
+          nextNode = comment;
+        } else if (nextToken && comment) {
+          nextNode = nextToken.range[0] < comment.range[0] ? nextToken : comment;
+        }
 
-          if (!isImportNext && nextToken.loc.start.line - node.loc.end.line !== 2) {
+        if (nextNode) {
+          const isImportNext = nextNode.type === 'Keyword' && nextNode.value === 'import';
+
+          if (!isImportNext && nextNode.loc.start.line - node.loc.end.line !== 2) {
             context.report({
               node,
               message: '1 empty line after imports is required',
               fix(fixer) {
-                return fixer.replaceTextRange([node.range[1], nextToken.range[0]], '\n\n');
+                return fixer.replaceTextRange([node.range[1], nextNode.range[0]], '\n\n');
               },
             });
           }
